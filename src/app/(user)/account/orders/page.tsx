1e76d22b52e93323c1ad6cadcceae96ec4f7569c
LeelaMohan217 <leelamohan217@gmail.com>
2026-06-26 15:01:44 +0530

    fix: correct JSX structure in orders page cancel button

--- src/app/(user)/account/orders/page.tsx
+++ src/app/(user)/account/orders/page.tsx
@@ -266,8 +266,7 @@ export default function OrdersPage() {
                         )}
                         </div>
                       )}
-                  </div>
-                )}
+                    </div>
               </div>
             );
           })}
